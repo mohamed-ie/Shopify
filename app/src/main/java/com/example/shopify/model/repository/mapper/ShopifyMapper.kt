44cfad6a492a040213cd456cef6cdbb3bea7c33e
hamed <hamed.arfat@icloud.com>
2023-06-03 20:55:56 +0300

    - Add Some imports

--- app/src/main/java/com/example/shopify/model/repository/mapper/ShopifyMapper.kt
+++ app/src/main/java/com/example/shopify/model/repository/mapper/ShopifyMapper.kt
@@ -1,6 +1,9 @@
 package com.example.shopify.model.repository.mapper
 
+import com.example.shopify.ui.screen.auth.login.model.SignInUserInfo
+import com.example.shopify.ui.screen.auth.login.model.SignInUserInfoResult
 import com.example.shopify.ui.screen.auth.registration.model.SignUpUserResponseInfo
+import com.example.shopify.ui.screen.home.model.Brand
 import com.shopify.buy3.GraphResponse
 import com.shopify.buy3.Storefront
 
