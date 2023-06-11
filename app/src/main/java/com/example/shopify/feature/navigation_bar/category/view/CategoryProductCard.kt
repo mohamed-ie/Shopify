d7db85378da540442a3f1d03d65b0111b20120bb
Rafeef <rafeefalmahy@gmail.com>
2023-06-11 11:40:52 +0300

    - design CategoryScreen - design CategoryProductCard

---
+++ app/src/main/java/com/example/shopify/feature/navigation_bar/category/view/CategoryProductCard.kt
@@ -0,0 +1,49 @@
+package com.example.shopify.feature.navigation_bar.category.view
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.shopify.feature.navigation_bar.home.screen.home.ImageFromUrl
+import com.example.shopify.feature.navigation_bar.home.screen.product.model.BrandProduct
+
+@Composable
+fun CategoryProductCard(product: BrandProduct, onItemClick: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(10.dp)
+            .clickable { onItemClick(product.id) },
+        shape = MaterialTheme.shapes.small,
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        border = BorderStroke(0.5.dp, MaterialTheme.colorScheme.primary.copy(alpha = 0.2f)),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 0.dp
+        )
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Box(modifier = Modifier.padding(4.dp)) {
+                ImageFromUrl(url = product.images[0])
+            }
+            Text(
+                product.title,
+                textAlign = TextAlign.Center,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.labelMedium,
+                modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp)
+            )
+        }
+    }
+}
