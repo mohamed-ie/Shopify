1ce15a707c49824ff6b3ab541a04a100c85e9954
Rafeef <rafeefalmahy@gmail.com>
2023-06-11 16:14:23 +0300

    add CategoryGraph

---
+++ app/src/main/java/com/example/shopify/feature/navigation_bar/model/remote/FireStore.kt
@@ -0,0 +1,11 @@
+package com.example.shopify.feature.navigation_bar.model.remote
+
+object FireStore {
+    const val PRODUCT_COLLECTION_PATH = "product"
+    const val REVIEW_COLLECTION_PATH = "Review"
+    const val CREATED_AT_REVIEW_FIELD_KEY = "createdAt"
+    const val DESCRIPTION_REVIEW_FIELD_KEY = "description"
+    const val RATE_REVIEW_FIELD_KEY = "rate"
+    const val REVIEW_CONTENT_REVIEW_FIELD_KEY = "review"
+    const val REVIEWER_REVIEW_FIELD_KEY = "reviewer"
+}
