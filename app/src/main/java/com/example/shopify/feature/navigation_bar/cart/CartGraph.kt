79c738fd1eef63aeb542650e46d810a7e8b9de24
Rafeef <rafeefalmahy@gmail.com>
2023-06-13 14:15:27 +0300

    - navigate to checkout screen - navigate to credit card screen

--- app/src/main/java/com/example/shopify/feature/navigation_bar/cart/CartGraph.kt
+++ app/src/main/java/com/example/shopify/feature/navigation_bar/cart/CartGraph.kt
@@ -2,6 +2,8 @@ package com.example.shopify.feature.navigation_bar.cart
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -13,6 +15,9 @@ import com.example.shopify.feature.common.ErrorScreen
 import com.example.shopify.feature.navigation_bar.NavigationBarGraph
 import com.example.shopify.feature.navigation_bar.NavigationBarScreen
 import com.example.shopify.feature.navigation_bar.cart.view.CartScreen
+import com.example.shopify.feature.navigation_bar.my_account.screens.order.OrderViewModel
+import com.example.shopify.feature.navigation_bar.my_account.screens.order.view.component.credit_card_payment.CreditCardInfoScreen
+import com.example.shopify.feature.navigation_bar.my_account.screens.order.view.component.order.checkout.view.CheckoutScreen
 
 fun NavGraphBuilder.cartNavigation(navController: NavController) {
     navigation(route = NavigationBarGraph.CART, startDestination = CartGraph.Cart.route) {
@@ -26,13 +31,36 @@ fun NavGraphBuilder.cartNavigation(navController: NavController) {
         composable(route = CartGraph.ERROR) {
             ErrorScreen { navController.popBackStack() }
         }
+        composable(route = CartGraph.CHECK_OUT) {
+            val parentEntry = remember(it) {
+                navController.getBackStackEntry(CartGraph.CHECK_OUT)
+            }
+            CheckoutScreen(viewModel = hiltViewModel(parentEntry),
+                navigateTo = navController::navigate,
+                back = { navController.popBackStack() }
+            )
+        }
+        composable(route = CartGraph.CREDIT_CARD) {
+            val parentEntry = remember(it) {
+                navController.getBackStackEntry(CartGraph.CHECK_OUT)
+            }
+            val viewModel: OrderViewModel = hiltViewModel(parentEntry)
+            CreditCardInfoScreen(
+                state = viewModel.creditCardInfoState.collectAsState().value,
+                onEvent = viewModel::onCreditCardEvent
+            )
+        }
 
         addressGraph(navController)
+        // checkoutGraph(navController)
     }
 }
 
 object CartGraph {
     const val ERROR = "ERROR"
+    const val CHECK_OUT = "CHECK_OUT"
+    const val CREDIT_CARD = "CREDIT_CARD"
+
     object Cart :
         NavigationBarScreen(route = "CART", name = R.string.cart, icon = Icons.Rounded.ShoppingCart)
 }
